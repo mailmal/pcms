a8de1b5585f534b9b843ed26467788943c96b8f5
d041927 <d041927@d041927-VirtualBox.(none)>
2013-03-09 09:42:07 +0100

    added overview and gitignores

--- public/js/galery.js
+++ public/js/galery.js
@@ -10,52 +10,99 @@ jQuery(function(){
 		var this_div = jQuery(this);
 		var name = this_div.children('.category_name').html();
 		categories[name] = this_div;
-                cat_array.push(name);
+                cat_array.unshift(name);
 	});
+
+
+	var grepper = /#(.+)$/;
+	var match = grepper.exec( document.location.href );
+	if( match ){
+	  var elements = match[1].split(/#/);
+	  switch_category(elements[0]);
+	  next_image(undefined,elements[1] );
+	}else{
+	  switch_category(cat_array[0]);
+	  next_image(undefined,0);
+	}
   
 });
 
 
-function next_image(back) {
+function next_image(back,picture) {
    
    var picture_slide  = jQuery('.picture_slide');
    //var real_content = jQuery('.real_content_picture');
    var current_cat_name = cat_array[current_cattegory];
    var real_content = categories[current_cat_name].find('.real_content_picture');
-   
-   if(back){
-      current_picture--;
+   if( picture != undefined ){
+      current_picture = picture;
    }else{
-      current_picture++;
-   }
-   if( current_picture >= real_content.length ){
-      current_picture = 0;
-   }else if( current_picture < 0) {
-      current_picture = real_content.length -1;
+     if(back){
+	current_picture--;
+     }else{
+	current_picture++;
+     }
+     if( current_picture >= real_content.length ){
+	current_picture = 0;
+     }else if( current_picture < 0) {
+	current_picture = real_content.length -1;
+     }
    }
    
    var next_pixture_element = jQuery( real_content[ current_picture ] ).children('img');
    var next_description = jQuery( real_content[ current_picture ] ).children('span').html();
-   
-   var current_element = picture_slide.children('img');
-   picture_slide.append( next_pixture_element.clone() );
+
+   var current_element = picture_slide.children();
+   var new_picture = jQuery('<div style="height:100%;width:100%"></div>').append( next_pixture_element.clone());
+   //new_picture.hide();
+   //new_picture.fadeIn();
+
+   picture_slide.append( new_picture );
    jQuery('.description').html( next_description );
    
    current_element.detach();
 }
 
-function switch_category(){
+function switch_category(name){
    current_cattegory++; 
    if( current_cattegory >= cat_array.length ){
       current_cattegory = 0;
    }else if( current_cattegory < 0) {
       current_cattegory = cat_array.length -1;
    }
-console.log(cat_array);
    var current_cat_name = cat_array[current_cattegory];
+   if(name){
+     current_cat_name = name;
+     current_cattegory = cat_array.indexOf(name);
+   }else{
+     current_picture = -1;
+     next_image();
+   }
    jQuery('.cat_link').html( current_cat_name );
-   current_picture = -1;
-   next_image();
-   
+
 }
+
+
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(elt /*, from*/) {
+    var len = this.length;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+      ? Math.ceil(from)
+      : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++) {
+      if (from in this &&
+	  this[from] === elt)
+	return from;
+    }
+    return -1;
+  };
+}
+
+
+
 
